56365c8a81384644f965d0dc7375a3b2e53153c6
Silkworm <silkwrom4687@gmail.com>
2022-06-10 00:45:12 +0900

    1st commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <HelloWorld title="Hello"
+      message="vーon"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
